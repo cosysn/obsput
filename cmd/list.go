44f9d5df614ef8f82976a206f67c5b01cc406e95
root <root@localhost.localdomain>
2026-02-12 21:58:40 +0800

    feat: implement list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,6 +1,12 @@
 package cmd
 
 import (
+	"fmt"
+	obsclient "obsput/pkg/obs"
+
+	"obsput/pkg/config"
+	"obsput/pkg/output"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,8 +14,46 @@ func NewListCommand() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List uploaded versions",
-		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Println("list command")
+		RunE: func(cmd *cobra.Command, args []string) error {
+			outputFormat, _ := cmd.Flags().GetString("output")
+
+			// Load config
+			cfg, err := config.Load(getConfigPath())
+			if err != nil {
+				return fmt.Errorf("load config failed: %v", err)
+			}
+
+			formatter := output.NewFormatter()
+
+			for name, obsCfg := range cfg.Configs {
+				cmd.Printf("[%s]\n", name)
+
+				client := obsclient.NewClient(obsCfg.Endpoint, obsCfg.Bucket, obsCfg.AK, obsCfg.SK)
+				versions, err := client.ListVersions("")
+				if err != nil {
+					cmd.Printf("Error: %v\n", err)
+					continue
+				}
+
+				items := make([]output.VersionItem, 0, len(versions))
+				for _, v := range versions {
+					items = append(items, output.VersionItem{
+						Version: v.Version,
+						Size:    v.Size,
+						Date:    v.Date,
+						Commit:  v.Commit,
+						URL:     v.URL,
+					})
+				}
+
+				if outputFormat == "json" {
+					formatter.PrintJSON(items)
+				} else {
+					formatter.PrintVersionTable(items)
+				}
+			}
+
+			return nil
 		},
 	}
 	cmd.Flags().StringP("output", "o", "table", "Output format (table/json)")
